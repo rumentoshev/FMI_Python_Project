0abd760551f67a3e2a74360bd93fa2a734f01b0d
rumentoshev <monkey23toshev@abv.bg>
2023-02-14 17:48:56 +0200

    Setting urls

---
+++ Bair_Budali/Bair_Budali/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Bair_Budali project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bair_Budali.settings')
+
+application = get_wsgi_application()
