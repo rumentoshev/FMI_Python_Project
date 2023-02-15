7f2e25a12b489dd9deb50ee57070a6b4476892d9
rumentoshev <monkey23toshev@abv.bg>
2023-02-15 13:55:18 +0200

    Log out + log in done

--- Bair_Budali/core/views.py
+++ Bair_Budali/core/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url = 'signin')
 
 
 def index(request):
@@ -27,8 +30,6 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username,email=email,password=password)
                 user.save()
-
-
                 user_model = User.objects.get(username = username)
                 new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
                 new_profile.save()
@@ -39,3 +40,26 @@ def signup(request):
 
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password = password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url = 'signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
