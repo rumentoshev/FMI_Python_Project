251bd44f3b7a6317fcf52133e9b96151609d0f84
rumentoshev <monkey23toshev@abv.bg>
2023-02-15 20:36:07 +0200

    Upload pic button and functions

--- Bair_Budali/core/models.py
+++ Bair_Budali/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
-
+import uuid
+from datetime import datetime
 # Create your models here.
 # Every time you touch this file make migrations
 
@@ -12,9 +12,19 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(
-        upload_to='profile_images', default='10-Blank-Profile-Picture-with-Hat.png')
+    profileimg = models.ImageField(upload_to='profile_images', default='10-Blank-Profile-Picture-with-Hat.png')
     location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default = datetime.now)
+    no_of_likes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.user
