fb85eedd48c4f82298b37f916373b83e7bfb0546
rumentoshev <monkey23toshev@abv.bg>
2023-02-16 11:57:37 +0200

    Feed fixed to see posts of people you follow

--- Bair_Budali/core/views.py
+++ Bair_Budali/core/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
+from itertools import chain
 # Create your views here.
 
 @login_required(login_url = 'signin')
@@ -11,8 +12,21 @@ def index(request):
     user_object = User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user = user_object)
 
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower = request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for username in user_following_list:
+        feed_lists = Post.objects.filter(user = username)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
     posts = Post.objects.all()
-    return render(request, 'index.html',{'user_profile': user_profile, 'posts': posts})
+    return render(request, 'index.html',{'user_profile': user_profile, 'posts': feed_list})
 
 
 @login_required(login_url = 'signin')
