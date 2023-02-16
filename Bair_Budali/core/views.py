bc284254c9de8f25c7ac5103c336a7bf15e8246a
rumentoshev <monkey23toshev@abv.bg>
2023-02-16 09:57:39 +0200

    Profile page set up + some other things

--- Bair_Budali/core/views.py
+++ Bair_Budali/core/views.py
@@ -50,6 +50,20 @@ def like_post(request):
         post.save()
         return redirect('/')
 
+@login_required(login_url = 'signin')
+def profile(request, pk):
+    user_object = User.objects.get(username = pk)
+    user_profile = Profile.objects.get(user = user_object)
+    user_posts = Post.objects.filter(user = pk)
+    user_post_lenght = len(user_posts)
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_lenght': user_post_lenght,
+    }
+    return render(request, 'profile.html', context)
+
 
 @login_required(login_url = 'signin')
 def settings(request):
