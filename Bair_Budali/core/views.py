eaeb54fdf1cf42def6a7729c7ac0ad9d2f47952c
rumentoshev <monkey23toshev@abv.bg>
2023-02-16 10:57:42 +0200

    Follow/Unfollow done

--- Bair_Budali/core/views.py
+++ Bair_Budali/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -56,13 +56,45 @@ def profile(request, pk):
     user_profile = Profile.objects.get(user = user_object)
     user_posts = Post.objects.filter(user = pk)
     user_post_lenght = len(user_posts)
+
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower = follower, user = user).first():
+        button_text = 'Отследвай'
+    else:
+        button_text = 'Последвай'
+
+    user_followers = len(FollowersCount.objects.filter(user = pk))
+    user_following = len(FollowersCount.objects.filter(follower = pk))
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_lenght': user_post_lenght,
+        'button_text': button_text,
+        'user_followers': user_followers,
+        'user_following': user_following,
     }
     return render(request, 'profile.html', context)
+
+@login_required(login_url = 'signin')
+def follow(request):
+    
+    if request.method == "POST":
+        follower = request.POST['follower']
+        user = request.POST['user']
+        if FollowersCount.objects.filter(follower = follower, user = user).first():
+            delete_follower = FollowersCount.objects.get(follower = follower, user = user)
+            delete_follower.delete()
+            return redirect('/profile/' + user)
+        else:
+            new_followe = FollowersCount.objects.create(follower = follower, user = user)
+            new_followe.save()
+            return redirect('/profile/' + user)
+    else:
+        return redirect('/')
 
 
 @login_required(login_url = 'signin')
